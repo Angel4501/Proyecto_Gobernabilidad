37cc8b46bf68a6db3cf283acc6eb9c557307c902
Ángel Daniel Ponce Medina <adponce@unitec.edu>
2023-06-17 09:37:55 -0600

    La RAM solo dura hasta que se apaga la compu - Paulo Coelho

--- src/Administrador/Administrador.jsx
+++ src/Administrador/Administrador.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Administrador.css'
+import axios from 'axios';
+
 
 const Administrador = () => {
+    
 
+    const [textareaValue, setTextareaValue] = useState('');
+
+    const handleTextareaChange = (event) => {
+        setTextareaValue(event.target.value);
+    };
+
+    const handleButtonClick = async () => {
+        try {
+          const url = 'https://npr3s.com/control/projects/gti-1265-1167/issues.xml';
+          const apiKey = '3141e26b74ff2bcdcd1df91632cdbefb126be450'; // Reemplaza con tu clave de API
+          const data = {
+            issue: {
+              project_id: 19,
+              subject: 'ENVIADO DESDE APP',
+              description: 'COMENTARIO',
+              startdate: '2023-06-16'
+            }
+          };
+      
+          const response = await axios.post(url,{
+            headers: {
+              'Content-Type': 'application/json',
+              'X-Redmine-API-Key': apiKey
+            }
+          });
+      
+          console.log(response.data); // Respuesta del servidor
+        } catch (error) {
+          console.error(error);
+        }
+      };
 
     return (
         <div className="App">
@@ -11,9 +45,21 @@ const Administrador = () => {
                 <textarea
                     className="textAreaResponderPregunta"
                     placeholder="PRUEBA DESDE LA APP"
+                    onChange={handleTextareaChange}
                 />
                 <div>
-                    <button type="submit" className="submitButton">
+                    <button  className="submitButton" onClick={(e)=>{
+                        if(textareaValue===''){
+                            window.alert('No puede mandar un mensaje vacío')
+                        }
+                        else{
+                            //window.alert(textareaValue)
+                            handleButtonClick()
+                            window.alert('siuuu')
+                        }
+                        
+                        e.preventDefault()
+                    }}>
                         Enviar reporte
                     </button>
                 </div>
